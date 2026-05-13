77b15642041b3bdb22432ced9ad566f0e5f8caff
ipax77 <ph.ilipp@web.de>
2026-05-13 19:09:55 +0200

    add memory sample

---
+++ src/samples/Sc2DirectStrike.MemorySample/Program.cs
@@ -0,0 +1,24 @@
+﻿using s2protocol.NET;
+using Sc2DirectStrike.Parser;
+
+namespace Sc2DirectStrike.MemorySample;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var testReplay = @"C:\Users\pax77\source\repos\Sc2DirectStrike\src\Sc2DirectStrike.Tests\testdata\Direct Strike TE (1106).SC2Replay";
+        var replayDecoder = new ReplayDecoder();
+        var sc2Replay = await replayDecoder.DecodeAsync(testReplay);
+        ArgumentNullException.ThrowIfNull(sc2Replay);
+
+        Console.WriteLine("ready to parse.");
+        Console.ReadLine();
+
+        var dsReplay = Sc2DirectStrikeParser.Parse(sc2Replay);
+        ArgumentNullException.ThrowIfNull(dsReplay);
+
+        Console.WriteLine("job done.");
+        Console.ReadLine();
+    }
+}
